feba8a6eaecca5e86599789899cc375a5d241309
yuramishin <mishin.inbox@gmail.com>
2018-06-09 08:29:30 +0300

    Вычислить квадрат числа.

---
+++ chapter_001/src/main/java/ru/mishinyura/sqrt/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package sqrt.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 09.06.2018
+ */
+package ru.mishinyura.sqrt;
---
+++ chapter_001/src/test/java/ru/mishinyura/sqrt/SquareTest.java
@@ -0,0 +1,24 @@
+package ru.mishinyura.sqrt;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Класс SquareTest тестирует класс Square.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 09.06.2018
+ */
+public class SquareTest {
+    /**
+     * Тест метода...
+     */
+    @Test
+    public void whenNumberThenSquare() {
+        Square square = new Square();
+
+        assertThat(square.sqr(2), is(4));
+    }
+}
