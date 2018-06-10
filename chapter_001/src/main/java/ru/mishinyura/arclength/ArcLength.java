9ce0cee7bbf055f6f83b433764fc5b0de20914b1
Mishin Yura <mishin.inbox@gmail.com>
2018-06-10 11:34:51 +0300

    Расчёт длины окружности. Правка[#1]

--- chapter_001/src/main/java/ru/mishinyura/arclength/ArcLength.java
+++ chapter_001/src/main/java/ru/mishinyura/arclength/ArcLength.java
@@ -6,20 +6,14 @@ package ru.mishinyura.arclength;
  * @author Mishin Yura (mishin.inbox@gmail.com)
  * @since 10.06.2018
  */
-public final class ArcLength {
-    /**
-     * Конструктор.
-     */
-    private ArcLength() {
-    }
-
+public class ArcLength {
     /**
      * Метод вычисляет длину окружности.
      *
      * @param radius Радиус.
      * @return Длина.
      */
-    public static double circleLength(final int radius) {
+    public final double circleLength(final int radius) {
         return 2 * Math.PI * radius;
     }
 }
