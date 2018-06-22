5cd3c23f5761584b881640afe74139d1bad89cc6
Mishin Yura <mishin.inbox@gmail.com>
2018-06-22 15:07:07 +0300

    Calculate the average

---
+++ chapter_001/src/main/java/ru/mishinyura/whilefor/Avg.java
@@ -0,0 +1,27 @@
+package ru.mishinyura.whilefor;
+
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class Avg {
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(System.in));
+//
+//        boolean isExit = false;
+//        int number = 0, sum = 0, count = 0;
+//        double avg = 0;
+//
+//        while (!isExit) {
+//            number = Integer.parseInt(reader.readLine());
+//            if (number != -1) {
+//                count++;
+//                sum += number;
+//            } else {
+//                avg = (double) sum / count;
+//                isExit = true;
+//            }
+//        }
+//        System.out.println(avg);
+//    }
+//}
