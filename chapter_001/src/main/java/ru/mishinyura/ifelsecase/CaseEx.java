043cff048b57c9e40c08d96e22ff85c714bb2ddc
Mishin Yura <mishin.inbox@gmail.com>
2018-06-22 08:20:12 +0300

    Case example.

---
+++ chapter_001/src/main/java/ru/mishinyura/ifelsecase/CaseEx.java
@@ -0,0 +1,43 @@
+package ru.mishinyura.ifelsecase;
+
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class CaseEx {
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(System.in));
+//
+//        String day = reader.readLine();
+//        String result;
+//
+//        switch (day) {
+//            case "1":
+//                result = "понедельник";
+//                break;
+//            case "2":
+//                result = "вторник";
+//                break;
+//            case "3":
+//                result = "среда";
+//                break;
+//            case "4":
+//                result = "четверг";
+//                break;
+//            case "5":
+//                result = "пятница";
+//                break;
+//            case "6":
+//                result = "суббота";
+//                break;
+//            case "7":
+//                result = "воскресенье";
+//                break;
+//            default:
+//                result = "такого дня недели не существует";
+//                break;
+//        }
+//        System.out.println(result);
+//        reader.close();
+//    }
+//}
