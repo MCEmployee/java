4f6bf61642392cd9ab1dfe79bf46195217b886c1
Mishin Yura <mishin.inbox@gmail.com>
2018-06-22 08:32:55 +0300

    While Example.

---
+++ chapter_001/src/main/java/ru/mishinyura/whilefor/WhileEx.java
@@ -0,0 +1,18 @@
+package ru.mishinyura.whilefor;
+
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class WhileEx {
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(System.in));
+//        int number = 0, sum = 0;
+//
+//        while (number != -1) {
+//            number = Integer.parseInt(reader.readLine());
+//            sum += number;
+//        }
+//        System.out.println(sum);
+//    }
+//}
---
+++ chapter_001/src/main/java/ru/mishinyura/whilefor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for whilefor.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 04.06.2018
+ */
+package ru.mishinyura.whilefor;
